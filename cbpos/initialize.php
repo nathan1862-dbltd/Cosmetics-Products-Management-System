d759bacaa57d964d31a969d7df87b37ef539425d
nathan1862-dbltd <kaungkaung1862@icloud.com>
2026-05-24 21:09:58 +0630

    Update initialize.php

--- cbpos/initialize.php
+++ cbpos/initialize.php
@@ -1,10 +1,10 @@
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Robi','lastname'=>'Tanim','username'=>'RobiTanim','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://localhost/cbpos/');
+if(!defined('base_url')) define('base_url','http://cos.thebizportwebs.online/cbpos/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 // if(!defined('dev_data')) define('dev_data',$dev_data);
-if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
-if(!defined('DB_NAME')) define('DB_NAME',"cbpos_db");
+if(!defined('DB_SERVER')) define('DB_SERVER','localhost');
+if(!defined('DB_USERNAME')) define('DB_USERNAME','thebertg_admin');
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD','thebertg_admin');
+if(!defined('DB_NAME')) define('DB_NAME','thebertg_cos');
 ?>
