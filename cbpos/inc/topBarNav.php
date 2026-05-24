582c8e197ed53726134a30d08dbf7ae509e331d3
nathan1862-dbltd <kaungkaung1862@icloud.com>
2026-05-25 01:05:44 +0630

    Apply white header theme and icon-based account/cart actions

--- cbpos/inc/topBarNav.php
+++ cbpos/inc/topBarNav.php
@@ -1,24 +1,23 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-gradient-pink cpms-topbar">
+<nav class="navbar navbar-expand-lg cpms-topbar">
   <div class="container px-3 px-lg-5">
-    <div class="d-flex align-items-center w-100">
-      <a class="navbar-brand mr-2 cpms-topbar__brand" href="./">
-        <img src="<?php echo validate_image($_settings->info('logo')) ?>" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
-        <?php echo $_settings->info('short_name') ?>
+    <div class="d-flex align-items-center w-100 cpms-topbar__row">
+      <a class="navbar-brand mr-2 cpms-topbar__brand" href="./" aria-label="Delux Beauti Home">
+        <img src="<?php echo base_url ?>assets/img/delux-beauti-logo.png" class="cpms-topbar__brand-logo" alt="Delux Beauti" loading="lazy">
       </a>
 
       <form class="form-inline flex-grow-1 mx-2 cpms-topbar__search" id="search-form">
         <div class="input-group w-100">
           <input class="form-control form-control-sm" type="search" placeholder="Search" aria-label="Search" name="search" value="<?php echo isset($_GET['search']) ? $_GET['search'] : "" ?>" aria-describedby="button-addon2">
           <div class="input-group-append">
-            <button class="btn btn-outline-light btn-sm m-0" type="submit" id="button-addon2"><i class="fa fa-search"></i></button>
+            <button class="btn btn-sm m-0" type="submit" id="button-addon2"><i class="fa fa-search"></i></button>
           </div>
         </div>
       </form>
 
       <?php if($_settings->userdata('id') > 0 && $_settings->userdata('login_type') == 2): ?>
-      <a class="nav-link text-white d-lg-none p-0 cpms-topbar__mobile-cart" href="./?p=cart" aria-label="Cart">
+      <a class="nav-link d-lg-none p-0 cpms-topbar__mobile-cart" href="./?p=cart" aria-label="Cart">
         <i class="bi-cart-fill"></i>
-        <span class="badge bg-dark text-white rounded-pill" id="cart-count-mobile">
+        <span class="badge rounded-pill" id="cart-count-mobile">
           <?php 
             $count = $conn->query("SELECT SUM(quantity) as items from `cart` where client_id =".$_settings->userdata('id'))->fetch_assoc()['items'];
             echo ($count > 0 ? $count : 0);
@@ -26,7 +25,7 @@
         </span>
       </a>
       <?php else: ?>
-      <button class="btn btn-outline-dark btn-sm ml-2 d-lg-none" id="login-btn" type="button">Login</button>
+      <button class="btn btn-sm ml-2 d-lg-none cpms-topbar__login-btn" id="login-btn" type="button">Login</button>
       <?php endif; ?>
     </div>
 
@@ -34,33 +33,30 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto mb-2 mb-lg-0 ml-lg-4">
-        <li class="nav-item"><a class="nav-link text-white" aria-current="page" href="./">Home</a></li>
+        <li class="nav-item"><a class="nav-link" aria-current="page" href="./">Home</a></li>
         <?php 
         $cat_qry = $conn->query("SELECT * FROM categories where status = 1  limit 3");
         $count_cats =$conn->query("SELECT * FROM categories where status = 1 ")->num_rows;
         while($crow = $cat_qry->fetch_assoc()):
         ?>
-        <li class="nav-item"><a class="nav-link text-white" aria-current="page" href="./?p=products&c=<?php echo md5($crow['id']) ?>"><?php echo $crow['category'] ?></a></li>
+        <li class="nav-item"><a class="nav-link" aria-current="page" href="./?p=products&c=<?php echo md5($crow['id']) ?>"><?php echo $crow['category'] ?></a></li>
         <?php endwhile; ?>
         <?php if($count_cats > 3): ?>
-        <li class="nav-item"><a class="nav-link text-white" href="./?p=view_categories">All Categories</a></li>
+        <li class="nav-item"><a class="nav-link" href="./?p=view_categories">All Categories</a></li>
         <?php endif; ?>
-        <li class="nav-item"><a class="nav-link text-white" href="./?p=about">About</a></li>
+        <li class="nav-item"><a class="nav-link" href="./?p=about">About</a></li>
       </ul>
 
-      <div class="d-none d-lg-flex align-items-center">
+      <div class="d-none d-lg-flex align-items-center cpms-topbar__desktop-actions">
         <?php if($_settings->userdata('id') > 0 && $_settings->userdata('login_type') == 2): ?>
-          <a class="text-dark mr-3 nav-link text-white" href="./?p=cart">
+          <a class="mr-3 nav-link" href="./?p=cart" aria-label="Cart">
             <i class="bi-cart-fill mr-1"></i>
-            Cart
-            <span class="badge bg-dark text-white ml-1 rounded-pill" id="cart-count">
-              <?php echo ($count > 0 ? $count : 0); ?>
-            </span>
+            <span class="badge ml-1 rounded-pill" id="cart-count"><?php echo ($count > 0 ? $count : 0); ?></span>
           </a>
-          <a href="./?p=my_account" class="text-dark nav-link text-white"><b>Hi, <?php echo $_settings->userdata('firstname')?>!</b></a>
-          <a href="logout.php" class="text-dark nav-link text-white"><i class="fa fa-sign-out-alt"></i></a>
+          <a href="./?p=my_account" class="nav-link" aria-label="My Account"><i class="fa fa-user-circle mr-1"></i> My Account</a>
+          <a href="logout.php" class="nav-link" aria-label="Logout"><i class="fa fa-sign-out-alt"></i></a>
         <?php else: ?>
-          <button class="btn btn-outline-dark ml-2" id="login-btn-desktop" type="button">Login</button>
+          <button class="btn btn-sm cpms-topbar__login-btn" id="login-btn-desktop" type="button">Login</button>
         <?php endif; ?>
       </div>
     </div>
