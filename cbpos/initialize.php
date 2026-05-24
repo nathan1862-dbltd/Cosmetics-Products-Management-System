db3b32c4c583635c3f108cd6429eae0ec23aab17
nathan1862-dbltd <kaungkaung1862@icloud.com>
2026-05-24 21:18:25 +0630

    Update initialize.php

--- cbpos/initialize.php
+++ cbpos/initialize.php
@@ -1,6 +1,6 @@
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Robi','lastname'=>'Tanim','username'=>'RobiTanim','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://cos.thebizportwebs.online/cbpos/');
+if(!defined('base_url')) define('base_url','http://cos.thebizportwebs.online/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 // if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER','localhost');
