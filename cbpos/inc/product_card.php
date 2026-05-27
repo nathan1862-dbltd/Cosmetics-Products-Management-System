7f8f6787c0ff75760fe116ea11ad76bf372496c1
nathan1862-dbltd <kaungkaung1862@icloud.com>
2026-05-28 03:25:57 +0630

    Update product_card.php

--- cbpos/inc/product_card.php
+++ cbpos/inc/product_card.php
@@ -5,14 +5,28 @@
  * - $product_id, $product_name, $product_brand, $product_category, $product_price, $product_image
  */
 ?>
-<a class="card product-item product-card text-reset text-decoration-none h-100" href=".?p=view_product&id=<?php echo md5($product_id) ?>">
+<a class="card product-item product-card text-reset text-decoration-none h-100"
+   href=".?p=view_product&id=<?php echo md5($product_id) ?>">
+
     <div class="overflow-hidden shadow-sm product-holder product-card-media">
-        <img class="card-img-top w-100 product-cover" src="<?php echo validate_image($product_image) ?>" alt="<?php echo htmlspecialchars($product_name, ENT_QUOTES) ?>" />
+        <img class="card-img-top w-100 product-cover"
+             src="<?php echo validate_image($product_image) ?>"
+             alt="<?php echo htmlspecialchars($product_name, ENT_QUOTES) ?>" />
     </div>
+
     <div class="card-body p-3 d-flex flex-column">
-        <h5 class="fw-bolder product-title mb-2"><?php echo $product_name ?></h5>
-        <span class="mb-2"><b class="text-muted">Price: </b><?php echo $product_price ?></span>
-        <p class="m-0"><small><span class="text-muted">Brand:</span> <?php echo $product_brand ?></small></p>
-        
+
+        <p class="brand">
+            <?php echo $product_brand ?>
+        </p>
+
+        <h5 class="product-title">
+            <?php echo $product_name ?>
+        </h5>
+
+        <span class="product-price">
+            <?php echo $product_price ?>
+        </span>
+
     </div>
 </a>
