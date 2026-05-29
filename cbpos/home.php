28ee471a5c6f0b093da16c75c3ca231da3353d07
nathan1862-dbltd <kaungkaung1862@icloud.com>
2026-05-29 15:28:09 +0630

    Update home.php

--- cbpos/home.php
+++ cbpos/home.php
@@ -96,3 +96,7 @@ $brands = isset($_GET['b']) ? json_decode(urldecode($_GET['b'])) : array();
     })
 
 </script>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
