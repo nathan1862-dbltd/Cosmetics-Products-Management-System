b30aa5ae27a2720f714d1c1cbd81d5dc505321b4
nathan1862-dbltd <kaungkaung1862@icloud.com>
2026-05-30 04:06:11 +0630

    Update product_slider.php

--- cbpos/inc/product_slider.php
+++ cbpos/inc/product_slider.php
@@ -70,3 +70,32 @@ $chunked_products = array_chunk($products, 4);
 
     </div>
 </div>
+
+<script>
+document.querySelectorAll('.product-slider-track').forEach(slider => {
+
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', e => {
+        isDown = true;
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+    });
+
+    slider.addEventListener('mouseleave', () => isDown = false);
+    slider.addEventListener('mouseup', () => isDown = false);
+
+    slider.addEventListener('mousemove', e => {
+        if(!isDown) return;
+
+        e.preventDefault();
+
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 2;
+
+        slider.scrollLeft = scrollLeft - walk;
+    });
+});
+</script>
